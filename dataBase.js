645aee20784756223af2bba693e9fd899ce79ab6
juan pablo bustamante <bustamante.jpm@gmail.com>
2023-05-11 07:57:06 -0300

    fix problems with db collums, fixed express server issues

--- dataBase.js
+++ dataBase.js
@@ -1,11 +1,11 @@
-const {pool} = require('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'finInstitution',
-    password: 'castelli123',
-    port: 5432,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'finInstitution',
+  password: 'castelli123',
+  port: 5432,
 });
 
 module.exports = pool;
